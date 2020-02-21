068b3ed199685a177582db3103d3ccd6da82e857
joaocruz19 <joaovictorcruz82@gmail.com>
2020-02-20 22:21:53 -0300

    Initial commit :tada:

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import "font-awesome/css/font-awesome.min.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Quero!</h1>
+    </div>
+  );
+}
+
+export default App;
