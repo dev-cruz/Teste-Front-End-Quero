042b3e6d420b8360b8cad3dffc8fc0dd9a88d700
joaocruz19 <joaovictorcruz82@gmail.com>
2020-03-02 10:19:03 -0300

    Filters helpers added

--- src/components/Content/ScholarshipDashboard/ScholarshipOptions/index.jsx
+++ src/components/Content/ScholarshipDashboard/ScholarshipOptions/index.jsx
@@ -1,38 +1,91 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import './styles.css';
 
 function ScholarshipOptions({ data, modalFilters }) {
+    const [filteredData, setFilteredData] = useState([]);
+
+    const filterByCity = scholarships => {
+        if (modalFilters.city === 'all') return scholarships;
+        return scholarships.filter(scholarship => scholarship.campus.city === modalFilters.city);
+    };
+    
+    const filterByCourse = scholarships => {
+        if (modalFilters.course === 'all') return scholarships;
+        return scholarships.filter(scholarship => scholarship.course.name === modalFilters.course);
+    };
+    
+    const filterByPresential = scholarships => {
+        if (!modalFilters.presential) {
+            return scholarships.filter(scholarship => scholarship.course.kind !== 'Presencial');
+        }
+        return scholarships;
+    };
+    
+    const filterByRemotely = scholarships => {
+        if (!modalFilters.remotely) {
+            return scholarships.filter(scholarship => scholarship.course.kind !== 'EaD');
+        }
+        return scholarships;
+    };
+    
+    const filterByPrice = scholarships =>
+        scholarships.filter(scholarship => scholarship.price_with_discount <= modalFilters.price);
+
+    function filterScholarships() {
+        return new Promise(resolve => {
+            resolve(data);
+        })
+    };
+
+    async function applyFilters() {
+        const initialData = await filterScholarships();
+        const filteredByCity = await filterByCity(initialData);
+        const filteredByCourse = await filterByCourse(filteredByCity);
+        const filteredByPresential = await filterByPresential(filteredByCourse);
+        const filteredByRemotely = await filterByRemotely(filteredByPresential);
+        const finalFilter = await filterByPrice(filteredByRemotely);
+        setFilteredData(finalFilter);
+    }
+
+    if(!data) {
+        return (
+            <p>Os dados não puderam ser carregados</p>
+        )
+    }
+    applyFilters();
     return (
         <div className="scholarship-options">
-            {data.map((data, index) => (
-                <>
-                    <hr className="divider"></hr>
-                    <div key={index} className="scholarship-option">
-                        <div className="scholarship-option__checkbox">
-                            <input type="checkbox" />
-                            <label></label>
-                        </div>
-                        <div className="scholarship-option__logo">
-                            <img src={data.university.logo_url} alt="" />
-                        </div>
-                        <div className="scholarship-option__description">
-                            <div className="description__title">
-                                <p className="title__course-name">{data.course.name}</p>
-                                <small className="title__course-level">{data.course.level}</small>
+            {
+                filteredData.map((data, index) => (
+                    <>
+                        <hr className="divider"></hr>
+                        <div key={index} className="scholarship-option">
+                            <div className="scholarship-option__checkbox">
+                                <input type="checkbox" />
+                                <label></label>
                             </div>
-                            <div className="description__price">
-                                <p>
-                                    Bolsa de <span className="price--discount-text">{data.discount_percentage}%</span>
-                                </p>
-                                <p className="price--discount-text">
-                                    R$ {parseInt(data.price_with_discount)}/mês
-                            </p>
+                            <div className="scholarship-option__logo">
+                                <img src={data.university.logo_url} alt="" />
+                            </div>
+                            <div className="scholarship-option__description">
+                                <div className="description__title">
+                                    <p className="title__course-name">{data.course.name}</p>
+                                    <small className="title__course-level">{data.course.level}</small>
+                                </div>
+                                <div className="description__price">
+                                    <p>
+                                        scholarship de <span className="price--discount-text">{data.discount_percentage}%</span>
+                                    </p>
+                                    <p className="price--discount-text">
+                                        R$ {parseInt(data.price_with_discount)}/mês
+                                            </p>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </>
-            ))}
+                    </>
+                ))
+            }
             <hr className="divider"></hr>
         </div>
     )
