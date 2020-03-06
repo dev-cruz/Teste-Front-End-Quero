1c2d62d1117e7915e199369ff922552bc4a703db
joaocruz19 <joaovictorcruz82@gmail.com>
2020-03-06 09:49:03 -0300

    Implementing filters

--- src/components/Content/ScholarshipDashboard/ScholarshipOptions/index.jsx
+++ src/components/Content/ScholarshipDashboard/ScholarshipOptions/index.jsx
@@ -1,46 +1,50 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import './styles.css';
+import React from "react";
+import { connect } from "react-redux";
+import "./styles.css";
 
-function ScholarshipOptions({ data, modalFilters }) {
-    return (
-        <div className="scholarship-options">
-            {data.map((data, index) => (
-                <>
-                    <hr className="divider"></hr>
-                    <div key={index} className="scholarship-option">
-                        <div className="scholarship-option__checkbox">
-                            <input type="checkbox" />
-                            <label></label>
-                        </div>
-                        <div className="scholarship-option__logo">
-                            <img src={data.university.logo_url} alt="" />
-                        </div>
-                        <div className="scholarship-option__description">
-                            <div className="description__title">
-                                <p className="title__course-name">{data.course.name}</p>
-                                <small className="title__course-level">{data.course.level}</small>
-                            </div>
-                            <div className="description__price">
-                                <p>
-                                    Bolsa de <span className="price--discount-text">{data.discount_percentage}%</span>
-                                </p>
-                                <p className="price--discount-text">
-                                    R$ {parseInt(data.price_with_discount)}/mês
-                            </p>
-                            </div>
-                        </div>
-                    </div>
-                </>
-            ))}
-            <hr className="divider"></hr>
-        </div>
-    )
+function ScholarshipOptions({ filteredData }) {
+  return (
+    <div className="scholarship-options">
+      {filteredData.map((data, index) => (
+        <>
+          <hr className="divider"></hr>
+          <div key={index} className="scholarship-option">
+            <div className="scholarship-option__checkbox">
+              <input type="checkbox" />
+              <label></label>
+            </div>
+            <div className="scholarship-option__logo">
+              <img src={data.university.logo_url} alt="" />
+            </div>
+            <div className="scholarship-option__description">
+              <div className="description__title">
+                <p className="title__course-name">{data.course.name}</p>
+                <small className="title__course-level">
+                  {data.course.level}
+                </small>
+              </div>
+              <div className="description__price">
+                <p>
+                  Bolsa de{" "}
+                  <span className="price--discount-text">
+                    {data.discount_percentage}%
+                  </span>
+                </p>
+                <p className="price--discount-text">
+                  R$ {parseInt(data.price_with_discount)}/mês
+                </p>
+              </div>
+            </div>
+          </div>
+        </>
+      ))}
+      <hr className="divider"></hr>
+    </div>
+  );
 }
 
 const mapStateToProps = state => ({
-    data: state.modal.data,
-    modalFilters: state.modal.modalFilters
-})
+  filteredData: state.modal.filteredData
+});
 
 export default connect(mapStateToProps)(ScholarshipOptions);
--- src/store/actions/modal.js
+++ src/store/actions/modal.js
@@ -1,51 +1,56 @@
 const ModalActions = {
-    updateCity(city) {
-        return {
-            type: 'UPDATE_CITY',
-            city
-        }
-    },
-    updateCourse(course) {
-        return {
-            type: 'UPDATE_COURSE',
-            course
-        }
-    },
-    updatePresential(presential) {
-        return {
-            type: 'UPDATE_PRESENTIAL',
-            presential
-        }
-    },
-    updateRemotely(remotely) {
-        return {
-            type: 'UPDATE_REMOTELY',
-            remotely
-        }
-    },
-    updatePrice(price) {
-        return {
-            type: 'UPDATE_PRICE',
-            price
-        }
-    },
-    updateData(data) {
-        return {
-            type: 'UPDATE_DATA',
-            data
-        }
-    },
-    setIsLoading(isLoading) {
-        return {
-            type: 'IS_LOADING',
-            isLoading
-        }
-    },
-    resetState() {
-        return {
-            type: 'RESET_STATE'
-        }
-    }
-}
+  updateCity(city) {
+    return {
+      type: "UPDATE_CITY",
+      city
+    };
+  },
+  updateCourse(course) {
+    return {
+      type: "UPDATE_COURSE",
+      course
+    };
+  },
+  updatePresential(presential) {
+    return {
+      type: "UPDATE_PRESENTIAL",
+      presential
+    };
+  },
+  updateRemotely(remotely) {
+    return {
+      type: "UPDATE_REMOTELY",
+      remotely
+    };
+  },
+  updatePrice(price) {
+    return {
+      type: "UPDATE_PRICE",
+      price
+    };
+  },
+  updateData(data) {
+    return {
+      type: "UPDATE_DATA",
+      data
+    };
+  },
+  setIsLoading(isLoading) {
+    return {
+      type: "IS_LOADING",
+      isLoading
+    };
+  },
+  resetState() {
+    return {
+      type: "RESET_STATE"
+    };
+  },
+  filterData() {
+    return {
+      type: "FILTER_DATA"
+    };
+  }
+};
 
 export default ModalActions;
